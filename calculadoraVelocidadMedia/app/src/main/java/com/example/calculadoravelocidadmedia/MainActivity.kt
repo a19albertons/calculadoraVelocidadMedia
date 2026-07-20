d63ac584f26cbd2b840f01a509662f59f36f74b7
A19AlbertoNS <a19albertons@iessanclemente.net>
2026-07-20 16:50:11 +0200

    Añadir comentarios y dokka

--- calculadoraVelocidadMedia/app/src/main/java/com/example/calculadoravelocidadmedia/MainActivity.kt
+++ calculadoraVelocidadMedia/app/src/main/java/com/example/calculadoravelocidadmedia/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.Observer
 import com.example.calculadoravelocidadmedia.databinding.ActivityMainBinding
 
+/**
+ * Actividad principal de la aplicación.
+ */
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     val binding: ActivityMainBinding
@@ -18,6 +21,9 @@ class MainActivity : AppCompatActivity() {
 
     val model: CalculadoraModel by viewModels()
 
+    /**
+     * Crea la actividad.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -29,6 +35,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        // Configura el observador de cantidad de km y actualiza el función de la string el text watcher
         binding.cantidadKm.addTextChangedListener(
             object : TextWatcher {
                 override fun afterTextChanged(s: Editable?) {
@@ -53,6 +60,7 @@ class MainActivity : AppCompatActivity() {
             },
         )
 
+        // Configura el observador de cantidad de horas y actualiza el función de la string el text watcher
         binding.cantidadHoras.addTextChangedListener(
             object : TextWatcher {
                 override fun afterTextChanged(s: Editable?) {
@@ -77,23 +85,29 @@ class MainActivity : AppCompatActivity() {
             },
         )
 
+        // Crea un observador para actualizar la vista y obtener las 2 medias de velocidad
         val observer =
             Observer<String> {
                 val km = model.kilometros.value?.toIntOrNull() ?: 0
                 val h = model.horas.value?.toIntOrNull() ?: 0
+                // Valida que el digito introducido en horas (divisor) no sea ilegal
                 if (h != 0) {
+                    // Si es legal muestra datos
                     val kmPorHora = km / h
                     val mPorSegundo = (km * 1000) / (h * 3600)
                     binding.kilometrosHora.text = kmPorHora.toString()
                     binding.metrosSegundo.text = mPorSegundo.toString()
                     binding.mensajeError.text = getString(R.string.error_message)
                 } else {
+                    // En caso de que sea 0 indica automaticamente como valores 0 para ambos calculso y un mensaje de error
                     binding.kilometrosHora.text = "0"
                     binding.metrosSegundo.text = "0"
                     binding.mensajeError.text = getString(R.string.error_message_alt)
                     // Toast.makeText(this, "Esta feo eso de indicar 0 horas o nada", Toast.LENGTH_LONG).show()
                 }
             }
+
+        // Configura los observadores
         model.kilometros.observe(this, observer)
         model.horas.observe(this, observer)
     }
